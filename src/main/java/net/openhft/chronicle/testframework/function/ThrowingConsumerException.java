23d4a7c2a1a4212ac5673170cc0ffa4b789629b2
Per Minborg <minborg@speedment.com>
2021-11-16 11:31:08 +0100

    Add serialVersionUID

--- src/main/java/net/openhft/chronicle/testframework/function/ThrowingConsumerException.java
+++ src/main/java/net/openhft/chronicle/testframework/function/ThrowingConsumerException.java
@@ -2,6 +2,8 @@ package net.openhft.chronicle.testframework.function;
 
 public class ThrowingConsumerException extends RuntimeException {
 
+    static final long serialVersionUID = -8237762538281151227L;
+
     public ThrowingConsumerException(Throwable cause) {
         super(cause);
     }
