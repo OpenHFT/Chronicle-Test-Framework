51d589407df5a78550a8b9712faafab95b80d40a
Per Minborg <minborg@speedment.com>
2021-11-18 08:30:05 +0100

    Internalize ProcessRunner

--- src/main/java/net/openhft/chronicle/testframework/process/ProcessRunner.java
+++ src/main/java/net/openhft/chronicle/testframework/process/ProcessRunner.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.testframework.process;
 
+import net.openhft.chronicle.testframework.internal.process.InternalProcessRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +17,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public enum ProcessRunner {
-    ;
+import static java.util.Objects.requireNonNull;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProcessRunner.class);
+public final class ProcessRunner {
+
+    // Suppresses default constructor, ensuring non-instantiability.
+    public ProcessRunner() {
+    }
 
     /**
      * Spawn a process running the main method of a specified class
@@ -30,6 +34,8 @@ public enum ProcessRunner {
      * @throws IOException if there is an error starting the process
      */
     public static Process runClass(Class<?> clazz, String... args) throws IOException {
+        requireNonNull(clazz);
+        requireNonNull(args);
         return runClass(clazz, new String[]{}, args);
     }
 
@@ -43,31 +49,10 @@ public enum ProcessRunner {
      * @throws IOException if there is an error starting the process
      */
     public static Process runClass(Class<?> clazz, String[] jvmArgs, String[] programArgs) throws IOException {
-        // Because Java17 must be run using various module flags, these must be propagated
-        // to the child processes
-        // https://stackoverflow.com/questions/1490869/how-to-get-vm-arguments-from-inside-of-java-application
-        final RuntimeMXBean runtimeMxBean = ManagementFactory.getRuntimeMXBean();
-
-        // filter out javaagent params, or this confuses the IntelliJ debugger
-        final List<String> jvmArgsWithoutJavaAgents = runtimeMxBean.getInputArguments().stream()
-                .filter(arg -> !arg.startsWith("-javaagent:"))
-                .filter(arg -> !arg.startsWith("-agentlib:"))
-                .collect(Collectors.toList());
-
-        String classPath = System.getProperty("java.class.path");
-        String className = clazz.getName();
-        String javaBin = findJavaBinPath().toString();
-        List<String> allArgs = new ArrayList<>();
-        allArgs.add(javaBin);
-        allArgs.addAll(jvmArgsWithoutJavaAgents);
-        allArgs.addAll(Arrays.asList(jvmArgs));
-        allArgs.add("-cp");
-        allArgs.add(classPath);
-        allArgs.add(className);
-        allArgs.addAll(Arrays.asList(programArgs));
-        ProcessBuilder processBuilder = new ProcessBuilder(allArgs.toArray(new String[]{}));
-//        processBuilder.inheritIO(); // this doesn't place nice with surefire
-        return processBuilder.start();
+        requireNonNull(clazz);
+        requireNonNull(jvmArgs);
+        requireNonNull(programArgs);
+        return InternalProcessRunner.runClass(clazz, jvmArgs, programArgs);
     }
 
     /**
@@ -78,51 +63,9 @@ public enum ProcessRunner {
      * https://maven.apache.org/surefire/maven-failsafe-plugin/faq.html#corruptedstream
      */
     public static void printProcessOutput(String processName, Process process) {
-        LOGGER.info("\n"
-                + "Output for " + processName + "\n"
-                + "stdout:\n"
-                + copyStreamToString(process.getInputStream()) + "\n"
-                + "stderr:\n"
-                + copyStreamToString(process.getErrorStream()));
+        requireNonNull(processName);
+        requireNonNull(process);
+        InternalProcessRunner.printProcessOutput(processName, process);
     }
 
-    /**
-     * Copies a stream to a string, up to the point where reading more would block
-     *
-     * @param inputStream The stream to read from
-     * @return The output as a string
-     */
-    private static String copyStreamToString(InputStream inputStream) {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int read;
-        try {
-            while (inputStream.available() > 0 && (read = inputStream.read(buffer)) >= 0) {
-                os.write(buffer, 0, read);
-            }
-        } catch (IOException e) {
-            // Ignore
-        }
-        return new String(os.toByteArray(), Charset.defaultCharset());
-    }
-
-    /**
-     * Try and work out what the java executable is cross platform
-     *
-     * @return the Path to the java executable
-     * @throws IllegalStateException if the executable couldn't be located
-     */
-    private static Path findJavaBinPath() {
-        final Path javaBinPath = Paths.get(System.getProperty("java.home")).resolve("bin");
-        final Path linuxJavaExecutable = javaBinPath.resolve("java");
-        if (linuxJavaExecutable.toFile().exists()) {
-            return linuxJavaExecutable;
-        } else {
-            Path windowsJavaExecutable = javaBinPath.resolve("java.exe");
-            if (windowsJavaExecutable.toFile().exists()) {
-                return windowsJavaExecutable;
-            }
-        }
-        throw new IllegalStateException("Couldn't locate java executable!");
-    }
 }
