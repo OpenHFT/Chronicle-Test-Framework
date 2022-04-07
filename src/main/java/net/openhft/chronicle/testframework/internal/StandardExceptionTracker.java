8bb771e2c2ad6153732710702d60b78ffa43f3a5
Nick Tindall <nick@tindall.id.au>
2022-04-08 09:32:03 +1000

    Finish StandardExceptionTracker

--- src/main/java/net/openhft/chronicle/testframework/internal/StandardExceptionTracker.java
+++ src/main/java/net/openhft/chronicle/testframework/internal/StandardExceptionTracker.java
@@ -1,6 +1,8 @@
 package net.openhft.chronicle.testframework.internal;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,21 +17,37 @@ import java.util.stream.Collectors;
  */
 public final class StandardExceptionTracker<T> implements ExceptionTracker<T> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StandardExceptionTracker.class);
+
     private final Map<Predicate<T>, String> ignoredExceptions = new LinkedHashMap<>();
     private final Map<Predicate<T>, String> expectedExceptions = new LinkedHashMap<>();
     private final Function<T, String> messageExtractor;
     private final Function<T, Throwable> throwableExtractor;
     private final Runnable resetRunnable;
     private final Map<T, Integer> exceptions;
+    private final Predicate<T> ignorePredicate;
+    private final Function<T, String> exceptionRenderer;
 
     public StandardExceptionTracker(@NotNull final Function<T, String> messageExtractor,
                                     @NotNull final Function<T, Throwable> throwableExtractor,
                                     @NotNull final Runnable resetRunnable,
-                                    @NotNull final Map<T, Integer> exceptions) {
+                                    @NotNull final Map<T, Integer> exceptions,
+                                    @NotNull final Predicate<T> ignorePredicate) {
+        this(messageExtractor, throwableExtractor, resetRunnable, exceptions, ignorePredicate, String::valueOf);
+    }
+
+    public StandardExceptionTracker(@NotNull final Function<T, String> messageExtractor,
+                                    @NotNull final Function<T, Throwable> throwableExtractor,
+                                    @NotNull final Runnable resetRunnable,
+                                    @NotNull final Map<T, Integer> exceptions,
+                                    @NotNull final Predicate<T> ignorePredicate,
+                                    @NotNull final Function<T, String> exceptionRenderer) {
         this.messageExtractor = messageExtractor;
         this.throwableExtractor = throwableExtractor;
         this.resetRunnable = resetRunnable;
         this.exceptions = exceptions;
+        this.ignorePredicate = ignorePredicate;
+        this.exceptionRenderer = exceptionRenderer;
     }
 
     @Override
@@ -61,20 +79,36 @@ public final class StandardExceptionTracker<T> implements ExceptionTracker<T> {
         expectedExceptions.clear();
         for (Map.Entry<Predicate<T>, String> ignoredException : ignoredExceptions.entrySet()) {
             if (!exceptions.keySet().removeIf(ignoredException.getKey()))
-                //Slf4jExceptionHandler.DEBUG.on(getClass(), "Ignored " + ignoredException.getValue());
-                System.err.println(getClass() + " DEBUG Ignored " + ignoredException.getValue());
+                LOGGER.debug("Ignored {}", ignoredException.getValue());
         }
         ignoredExceptions.clear();
-        /*
-        if (Jvm.hasException(exceptions)) {
-            Jvm.dumpException(exceptions);
-            TODO: Extract code from JVM an put it here...
 
-*/
+        if (hasExceptions()) {
+            dumpException();
+
             final String msg = exceptions.size() + " exceptions were detected: " + exceptions.keySet().stream().map(messageExtractor::apply).collect(Collectors.joining(", "));
             resetRunnable.run();
             throw new AssertionError(msg);
- /*       }*/
+        }
+    }
+
+    private boolean hasExceptions() {
+        for (T k : exceptions.keySet()) {
+            if (!ignorePredicate.test(k))
+                return true;
+        }
+
+        return false;
+    }
+
+    public void dumpException() {
+        for (@NotNull Map.Entry<T, Integer> entry : exceptions.entrySet()) {
+            final T key = entry.getKey();
+            LOGGER.warn(exceptionRenderer.apply(key), throwableExtractor.apply(key));
+            final Integer value = entry.getValue();
+            if (value > 1)
+                LOGGER.warn("Repeated {} times", value);
+        }
     }
 
     private static boolean contains(String text, String message) {
