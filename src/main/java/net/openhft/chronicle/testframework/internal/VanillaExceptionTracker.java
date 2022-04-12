59b5c15b148ed1b6f9dbfee64f78f1e0cfb4439a
Nick Tindall <nick@tindall.id.au>
2022-04-12 18:14:23 +1000

    Add ExceptionTracker interface (#17)
    * Add ExceptionTracker interface OpenHFT/Chronicle-Core#374
    * Add imp
    * Finish StandardExceptionTracker
    * StandardExceptionTracker -> VanillaExceptionTracker
    * Test VanillaExceptionTracker
    * Make lifecycle of VanillaExceptionTracker more explicit
    * Add static factory
    * Remove unnecessary clearing of expected/ignoredExceptions
    
    Co-authored-by: Per Minborg <minborg@speedment.com>

---
+++ src/main/java/net/openhft/chronicle/testframework/internal/VanillaExceptionTracker.java
@@ -0,0 +1,126 @@
+package net.openhft.chronicle.testframework.internal;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * A test utility class for recording and executing assertions about the presence (or absence) of exceptions
+ *
+ * @param <T> The class used to represent thrown exceptions
+ */
+public final class VanillaExceptionTracker<T> implements ExceptionTracker<T> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VanillaExceptionTracker.class);
+
+    private final Map<Predicate<T>, String> ignoredExceptions = new LinkedHashMap<>();
+    private final Map<Predicate<T>, String> expectedExceptions = new LinkedHashMap<>();
+    private final Function<T, String> messageExtractor;
+    private final Function<T, Throwable> throwableExtractor;
+    private final Runnable resetRunnable;
+    private final Map<T, Integer> exceptions;
+    private final Predicate<T> ignorePredicate;
+    private final Function<T, String> exceptionRenderer;
+    private boolean finalised = false;
+
+    public VanillaExceptionTracker(@NotNull final Function<T, String> messageExtractor,
+                                   @NotNull final Function<T, Throwable> throwableExtractor,
+                                   @NotNull final Runnable resetRunnable,
+                                   @NotNull final Map<T, Integer> exceptions,
+                                   @NotNull final Predicate<T> ignorePredicate) {
+        this(messageExtractor, throwableExtractor, resetRunnable, exceptions, ignorePredicate, String::valueOf);
+    }
+
+    public VanillaExceptionTracker(@NotNull final Function<T, String> messageExtractor,
+                                   @NotNull final Function<T, Throwable> throwableExtractor,
+                                   @NotNull final Runnable resetRunnable,
+                                   @NotNull final Map<T, Integer> exceptions,
+                                   @NotNull final Predicate<T> ignorePredicate,
+                                   @NotNull final Function<T, String> exceptionRenderer) {
+        this.messageExtractor = messageExtractor;
+        this.throwableExtractor = throwableExtractor;
+        this.resetRunnable = resetRunnable;
+        this.exceptions = exceptions;
+        this.ignorePredicate = ignorePredicate;
+        this.exceptionRenderer = exceptionRenderer;
+    }
+
+    @Override
+    public void expectException(String message) {
+        expectException(k -> contains(messageExtractor.apply(k), message) || (throwableExtractor.apply(k) != null && contains(throwableExtractor.apply(k).getMessage(), message)), message);
+    }
+
+    @Override
+    public void expectException(Predicate<T> predicate, String description) {
+        checkFinalised();
+        expectedExceptions.put(predicate, description);
+    }
+
+    @Override
+    public void ignoreException(String message) {
+        ignoreException(k -> contains(messageExtractor.apply(k), message) || (throwableExtractor.apply(k) != null && contains(throwableExtractor.apply(k).getMessage(), message)), message);
+    }
+
+    @Override
+    public void ignoreException(Predicate<T> predicate, String description) {
+        checkFinalised();
+        ignoredExceptions.put(predicate, description);
+    }
+
+    @Override
+    public void checkExceptions() {
+        checkFinalised();
+        finalised = true;
+        for (Map.Entry<Predicate<T>, String> expectedException : expectedExceptions.entrySet()) {
+            if (!exceptions.keySet().removeIf(expectedException.getKey()))
+                throw new AssertionError("No error for " + expectedException.getValue());
+        }
+        for (Map.Entry<Predicate<T>, String> ignoredException : ignoredExceptions.entrySet()) {
+            if (exceptions.keySet().removeIf(ignoredException.getKey()))
+                LOGGER.debug("Ignored {}", ignoredException.getValue());
+        }
+
+        if (hasExceptions()) {
+            dumpException();
+
+            final String msg = exceptions.size() + " exceptions were detected: " + exceptions.keySet().stream().map(messageExtractor::apply).collect(Collectors.joining(", "));
+            throw new AssertionError(msg);
+        }
+        resetRunnable.run();
+    }
+
+    private boolean hasExceptions() {
+        for (T k : exceptions.keySet()) {
+            if (!ignorePredicate.test(k))
+                return true;
+        }
+
+        return false;
+    }
+
+    private void dumpException() {
+        for (@NotNull Map.Entry<T, Integer> entry : exceptions.entrySet()) {
+            final T key = entry.getKey();
+            LOGGER.warn(exceptionRenderer.apply(key), throwableExtractor.apply(key));
+            final Integer value = entry.getValue();
+            if (value > 1)
+                LOGGER.warn("Repeated {} times", value);
+        }
+    }
+
+    private static boolean contains(String text, String message) {
+        return text != null && text.contains(message);
+    }
+
+    private void checkFinalised() {
+        if (finalised) {
+            throw new IllegalStateException("VanillaExceptionTracker is single use, you create it, add expectations/ignores, run tests, call check and then dispose of it.");
+        }
+    }
+}
---
+++ src/test/java/net/openhft/chronicle/testframework/internal/VanillaExceptionTrackerTest.java
@@ -0,0 +1,132 @@
+package net.openhft.chronicle.testframework.internal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class VanillaExceptionTrackerTest {
+
+    private AtomicInteger resetCounter = new AtomicInteger(0);
+    private Map<ExceptionHolder, Integer> exceptionCounts;
+    private VanillaExceptionTracker<ExceptionHolder> vet;
+
+    @BeforeEach
+    void setUp() {
+        resetCounter = new AtomicInteger(0);
+        exceptionCounts = new HashMap<>();
+        vet = new VanillaExceptionTracker<>(
+                ExceptionHolder::getDescription,
+                ExceptionHolder::getException,
+                () -> {
+                    resetCounter.incrementAndGet();
+                    exceptionCounts.clear();
+                },
+                exceptionCounts,
+                ExceptionHolder::isIgnore);
+    }
+
+    @Test
+    void noExceptionsWillPassCheck() {
+        vet.checkExceptions();
+    }
+
+    @Test
+    void expectedExceptionsWillNotFailCheck_Description() {
+        vet.expectException("foobar");
+        exceptionCounts.put(new ExceptionHolder("foobar", new IllegalStateException(), false), 1);
+        vet.checkExceptions();
+    }
+
+    @Test
+    void expectedExceptionsWillNotFailCheck_ThrowableMessage() {
+        vet.expectException("foobar");
+        exceptionCounts.put(new ExceptionHolder("something else", new IllegalStateException("foobar"), false), 1);
+        vet.checkExceptions();
+    }
+
+    @Test
+    void ignoredExceptionsWillNotFailCheck_Description() {
+        vet.ignoreException("foobar");
+        exceptionCounts.put(new ExceptionHolder("foobar", new IllegalStateException(), false), 1);
+        vet.checkExceptions();
+    }
+
+    @Test
+    void ignoredExceptionsWillNotFailCheck_ThrowableMessage() {
+        vet.ignoreException("foobar");
+        exceptionCounts.put(new ExceptionHolder("something else", new IllegalStateException("foobar"), false), 1);
+        vet.checkExceptions();
+    }
+
+    @Test
+    void missingExceptedExceptionsWillFailCheck() {
+        vet.expectException("foobar");
+        assertThrows(AssertionError.class, () -> vet.checkExceptions());
+    }
+
+    @Test
+    void unexpectedExceptionsWillFailCheck() {
+        exceptionCounts.put(new ExceptionHolder("unexpected exception", new IllegalStateException(), false), 1);
+        assertThrows(AssertionError.class, () -> vet.checkExceptions());
+    }
+
+    @Test
+    void exceptionsIgnoredByPredicateWillNotFailCheck() {
+        exceptionCounts.put(new ExceptionHolder("test test", new IllegalStateException(), true), 1);
+        vet.checkExceptions();
+    }
+
+    @Test
+    void resetRunnableIsCalledInCheck() {
+        vet.checkExceptions();
+        assertEquals(1, resetCounter.get());
+    }
+
+    @Test
+    void cannotIgnoreExceptionsAfterFinalised() {
+        vet.checkExceptions();
+        assertThrows(IllegalStateException.class, () -> vet.ignoreException("test"));
+    }
+
+    @Test
+    void cannotExpectExceptionsAfterFinalised() {
+        vet.checkExceptions();
+        assertThrows(IllegalStateException.class, () -> vet.expectException("test"));
+    }
+
+    @Test
+    void cannotCheckExceptionsAfterFinalised() {
+        vet.checkExceptions();
+        assertThrows(IllegalStateException.class, () -> vet.checkExceptions());
+    }
+
+    private static class ExceptionHolder {
+        private final String description;
+        private final Throwable exception;
+        private final boolean ignore;
+
+        private ExceptionHolder(String description, Throwable exception, boolean ignore) {
+            this.description = description;
+            this.exception = exception;
+            this.ignore = ignore;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public Throwable getException() {
+            return exception;
+        }
+
+        public boolean isIgnore() {
+            return ignore;
+        }
+    }
+}
