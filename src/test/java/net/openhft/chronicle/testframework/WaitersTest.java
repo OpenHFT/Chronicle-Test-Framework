dd88c5ed067d8ded710005ecb1f3b51cebae4f78
Nick Tindall <nick.tindall@chronicle.software>
2022-08-31 18:52:21 +1000

    Add Waiters

---
+++ src/test/java/net/openhft/chronicle/testframework/WaitersTest.java
@@ -0,0 +1,43 @@
+package net.openhft.chronicle.testframework;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class WaitersTest {
+
+    @Test
+    void willThrowWhenConditionIsNotMet() {
+        assertThrows(Waiters.ConditionNotMetException.class,
+                () -> Waiters.waitForCondition("Not met", () -> false, 30), "Not met");
+    }
+
+    @Test
+    void willReturnWhenConditionIsMet() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Waiters.waitForCondition("Met", () -> counter.incrementAndGet() > 2, 50);
+    }
+
+    @Test
+    void customMessagegGeneration() {
+        assertThrows(Waiters.ConditionNotMetException.class,
+                () -> Waiters.builder(() -> "value", value -> false)
+                        .messageGenerator(value -> "Wrong value: " + value)
+                        .maxTimeToWaitMs(30)
+                        .run(), "Wrong value: value");
+    }
+
+    @Test
+    void settingPollInterval() {
+        AtomicInteger counter = new AtomicInteger(0);
+        long startTime = System.currentTimeMillis();
+        Waiters.builder(() -> counter.incrementAndGet() > 2)
+                .checkIntervalMs(30)
+                .run();
+        final long elapsedTime = System.currentTimeMillis() - startTime;
+        assertTrue(elapsedTime >= 60, "Elapsed time was " + elapsedTime);
+    }
+}
