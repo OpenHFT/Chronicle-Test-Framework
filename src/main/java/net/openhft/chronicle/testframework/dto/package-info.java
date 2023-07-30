94f9004e4489f66f12ab5c72bced55829eff1ead
Sabrina Lawrey <sabrina.lawrey@chronicle.software>
2023-07-30 20:10:41 +0100

    added javadoc to dtotester

---
+++ src/main/java/net/openhft/chronicle/testframework/dto/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Provides interfaces and classes for testing Data Transfer Objects (DTOs) within
+ * the Chronicle Test Framework. This package includes mechanisms for building
+ * and configuring tests that validate and manipulate DTOs, including functionality
+ * for accessing, mutating, resetting, and validating DTOs.
+ *
+ * <p>The main interface provided is {@link net.openhft.chronicle.testframework.dto.DtoTester},
+ * which offers a fluent builder pattern for configuring tests on DTOs.
+ */
+package net.openhft.chronicle.testframework.dto;
