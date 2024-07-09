928ce47f425b4d134654de0828d260dfe0db2325
Jerry Shea <jerry.shea@chronicle.software>
2024-07-10 08:06:52 +1000

    Revert "Include protected classes children into BootstrapUtils.scanClasses output."
    This reverts commit 2227c58c8bab42382a9a4ad5a0197b66f055eba0.

--- src/main/java/net/openhft/chronicle/testframework/internal/BootstrapUtils.java
+++ src/main/java/net/openhft/chronicle/testframework/internal/BootstrapUtils.java
@@ -80,16 +80,11 @@ public class BootstrapUtils {
                 .map(JavaClass::getName)
                 .collect(Collectors.toSet());
 
-        Set<JavaClass> children = protectedClasses.stream()
-                .flatMap(cls -> cls.getAllSubclasses().stream())
-                .filter(BootstrapUtils::notProtected)
-                .collect(Collectors.toSet());
-
+        Set<JavaClass> visited = new HashSet<>();
         Set<JavaClass> candidates = protectedClasses.stream()
-                .flatMap(cls -> allReferrers(cls, coreNames, children).stream())
+                .flatMap(cls -> allReferrers(cls, coreNames, visited).stream())
                 .filter(BootstrapUtils::notProtected)
                 .collect(Collectors.toSet());
-
         Set<String> candidatesNames = candidates.stream()
                 .map(JavaClass::getName)
                 .collect(Collectors.toCollection(TreeSet::new));
