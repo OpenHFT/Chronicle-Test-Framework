50ad359d9b5fe3429d68aeaa81c75ebebc7404a4
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-25 14:29:23 +0100

    Add package info for internal DTO builders

---
+++ src/main/java/net/openhft/chronicle/testframework/internal/dto/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Contains internal builder and tester implementations used to verify DTO behaviour.
+ * <p>
+ * The classes in this package are not part of the public API and may change
+ * without notice. They supply the default implementation of the
+ * {@link net.openhft.chronicle.testframework.dto.DtoTester} builder and tester.
+ */
+package net.openhft.chronicle.testframework.internal.dto;
