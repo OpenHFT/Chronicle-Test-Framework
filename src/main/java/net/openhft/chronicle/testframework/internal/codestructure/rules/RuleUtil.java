afc21e82a4ea0e0927379d3ec478ee0147b04143
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-25 14:28:30 +0100

    Clarify INTERNAL_PACKAGE_REGEX usage

--- src/main/java/net/openhft/chronicle/testframework/internal/codestructure/rules/RuleUtil.java
+++ src/main/java/net/openhft/chronicle/testframework/internal/codestructure/rules/RuleUtil.java
@@ -2,6 +2,14 @@ package net.openhft.chronicle.testframework.internal.codestructure.rules;
 
 /**
  * Utility class for sharing rule logic.
+ * <p>
+ * The {@code INTERNAL_PACKAGE_REGEX} constant matches package names that
+ * contain {@code impl} or {@code internal}. It is currently used by
+ * {@link NonInternalClassesMustNotExtendInternalClassesRuleSupplier} to
+ * ensure non-internal classes do not extend implementation classes. Other
+ * rules may reuse this expression when they need to filter out internal
+ * packages.
+ * </p>
  */
 public enum RuleUtil {
     ;
