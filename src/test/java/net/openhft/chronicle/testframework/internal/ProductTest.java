e06b857f10c6eb5cf3f1006fe02f1a4c7fecd365
Peter Lawrey <peter.lawrey@chronicle.software>
2025-10-28 14:20:41 +0000

    Update copyright information to reflect current ownership

--- src/test/java/net/openhft/chronicle/testframework/internal/ProductTest.java
+++ src/test/java/net/openhft/chronicle/testframework/internal/ProductTest.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2006-2020, Speedment, Inc. All Rights Reserved.
+ * Copyright 2006-2025 chronicle.software
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); You may not
  * use this file except in compliance with the License. You may obtain a copy of
